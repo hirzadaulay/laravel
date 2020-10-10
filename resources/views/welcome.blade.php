f1485c1b643e9fbd12497750582787cd494d5820
hirzadaulay <69154546+hirzadaulay@users.noreply.github.com>
2020-10-10 10:25:24 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel percobaan
                 </div>
 
                 <div class="links">
